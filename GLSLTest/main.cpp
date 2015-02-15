454113f078c6554e4f42e41c344dba08b5ff80e3
Jerry <linjinrui@ztgame.com>
2015-02-15 20:49:55 +0800

    Initial Commit

---
+++ GLSLTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GLSLTest
+//
+//  Created by Jerry on 15-2-15.
+//  Copyright (c) 2015年 com.jerry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
