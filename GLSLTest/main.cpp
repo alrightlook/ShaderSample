0a408cd50aa1a3880a0f29fe1ea6becb05dd1793
Jerry <linjinrui@ztgame.com>
2015-06-21 21:36:36 +0800

    update to glsl diffuse lighting

--- GLSLTest/main.cpp
+++ GLSLTest/main.cpp
@@ -16,17 +16,11 @@
 #include "VertexArray.h"
 #include "Model.h"
 
-void render() {
-    //glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0, 0.0, 0.0);
-    glVertex3f(0.0f, 1.0f, -1.0);
-    glColor3f(0.0, 1.0, 0.0);
-    glVertex3f(1.0f, 0.0f, -1.0);
-    glColor3f(0.0, 0.0, 1.0);
-    glVertex3f(-1.0f, 0.0f, -1.0);
-    glEnd();
-}
+
+struct Matrix4f {
+    float m[4][4];
+};
+
 
 int main(int argc, const char * argv[])
 {
@@ -38,10 +32,13 @@ int main(int argc, const char * argv[])
         std::cout<<"Init success"<<std::endl;
     }
     
-    SDL_Window* window = SDL_CreateWindow("Hello World", 0, 0, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("Hello World", 0, 0, 480, 480, SDL_WINDOW_SHOWN);
     SDL_GLContext glctx = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, glctx);
     glewInit();
+    
+    gluPerspective(45.0f, 1.0, 1.0, 1000.0);
+    gluLookAt(0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
     
     string trianglevert = Utils::getInstance()->readFiles("triangle.vert");
     string trianglefrag = Utils::getInstance()->readFiles("triangle.frag");
@@ -58,7 +55,7 @@ int main(int argc, const char * argv[])
     model.LoadModel("model/monkey.blend");
     triangleshader.registerAttribute("VertexPosition", 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0, 0);
     triangleshader.registerAttribute("TexCoord", 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(3*sizeof(float)), 1);
-    
+    triangleshader.registerAttribute("NormalCoord", 3, GL_FLOAT, GL_TRUE, sizeof(Vertex), (void*)(5*sizeof(float)), 2);
     
 //
 //    VertexArray* vao = new VertexArray(triangleshader.getProgramID());
@@ -103,6 +100,13 @@ int main(int argc, const char * argv[])
     string linklog = triangleshader.getProgramInfoLog();
     cout<<"The linker log is:"<<linklog<<endl;
     triangleshader.use();
+    
+    triangleshader.registerUniform("lightPos");
+    triangleshader.registerUniform("lambient");
+    triangleshader.registerUniform("mambient");
+    triangleshader.setUniform3f("lightPos", 0.0f, 10.0f, 0.0f);
+    triangleshader.setUniform3f("lambient", 1.0f, 1.0f, 1.0f);
+    triangleshader.setUniform3f("mambient", 0.3f, 0.3f, 0.3f);
     
     Texture* tex = new Texture(GL_TEXTURE_2D,GL_TEXTURE0, "brick.png");
     tex->Load(triangleshader.getProgramID(), "texUnit", 0);
